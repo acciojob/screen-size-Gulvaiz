99b45cdc3cb53ee197cfffb8f7682c12ed9e6d12
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-05 13:36:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function updateSize(){
+let size = document.querySelector(".size")
+let width = window.innerWidth
+let height = window.innerHeight
+size.innerText = `Width: ${width} and Height: ${height}`
+}
+
+window.addEventListener("resize", updateSize)
+updateSize()
